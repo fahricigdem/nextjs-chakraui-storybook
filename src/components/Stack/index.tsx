58dacc06ea99dff1cf780ee6ce559b603e95798a
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-09-28 18:46:05 +0200

    Stack Component added

--- src/components/Stack/index.tsx
+++ src/components/Stack/index.tsx
@@ -1,27 +1,29 @@
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 import * as React from 'react';
 
-export interface numberOfChildren{
-    type:string,
-    defaultValue:number
-}
-
 export interface Props{
     spacing:number,
     direction:"row"|"column",
     wrap:boolean,
     children:ReactJSXElement[],
-
+    padding:"small"|"medium"|"large",
+ 
+    
 }
 
 
-function Stack({ children, spacing = 2, direction = "row", wrap = false }:Props) {
+function Stack({ children, spacing = 2, direction = "row", wrap = true ,padding="medium"}:Props) {
     const style:React.CSSProperties = {
       display: "flex",
       gap: `${spacing * 0.25}rem`,
       flexWrap: wrap ? "wrap" : "nowrap",
       flexDirection: direction,
+      padding:padding=="small" ? ".5em" : padding=="medium"?"1em":"1.5em",
+      alignItems:"center",
+      justifyContent:"center",
+      border:"solid 3px blue",
     }
+
     return <div style={style}>{children}</div>
   }
 
--- src/components/Stack/stories.tsx
+++ src/components/Stack/stories.tsx
@@ -1,21 +1,10 @@
 import Stack,{Props} from "."
 import { Story, Meta } from '@storybook/react';
 
-
 export default{
     title:"Stack",
     component:Stack,
-
 } as Meta
-
-const divStyle={ 
-    width: "15vw",
-    height: "15vh",
-    backgroundColor: "red",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-}
 
 const Template:Story<Props>=({...args})=>{
 
@@ -26,11 +15,37 @@ const Template:Story<Props>=({...args})=>{
             <div style={divStyle}>3</div>
             <div style={divStyle}>4</div>
             <div style={divStyle}>5</div>
-</Stack>)}
+        </Stack>
+)}
 
-export const Default=Template.bind({})
+export const Wrap=Template.bind({})
+
+export const NoWrap=Template.bind({})
+
+NoWrap.args={
+    //...Blue.args,
+    wrap : false,
+  }
+
+  export const Column=Template.bind({})
+
+Column.args={
+    //...Blue.args,
+    direction : "column",
+  }
 
 
 
+////style for div
 
+const divStyle:React.CSSProperties={ 
+    width: "18em",
+    height: "15vh",
+    backgroundColor: "red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius:"3%",
+    boxShadow: "2px 2px 2px 1px rgba(0, 0, 0, 0.2)",
 
+}
